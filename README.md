163c3a5385d97694d658a8de05f4ebd1bf8e1f90
ahmadneor1 <ahmadneor1@gmail.com>
2025-04-26 01:45:57 +0700

    first commit

--- README.md
+++ README.md
@@ -6,19 +6,18 @@ Welcome to the Ethereum AUTO Wallet Generator, a simple script to effortlessly g
 # Follow this step!
 
 1. First, you need to clone this repo:
-<pre lang="markdown">```https://github.com/ahmadneor1/evmgenerator.git```</pre>
+<pre lang="markdown">https://github.com/ahmadneor1/evmgenerator.git</pre>
 2. After that, go to the folder:
-<pre lang="markdown">```cd evmgenerator```</pre>
+<pre lang="markdown">cd evmgenerator</pre>
 3. Then, you need to install it:
-   <pre lang="markdown">```npm install```</pre>
+   <pre lang="markdown">npm install</pre>
    or
-   <pre lang="markdown">```npm i```</pre>
+   <pre lang="markdown">npm i</pre>
 4. Finally, you can run it:
-   <pre lang="markdown">```node main.js```</pre>
+   <pre lang="markdown">node main.js</pre>
    Put the amount that you want, example: 5 wallets => Input "5".
 
 
 # Prerequisites:
 
 Make sure you have Node.js installed on your computer. If not, you can download it here: https://nodejs.org/en.
-# evmgenerator
---
+++ main.js
@@ -0,0 +1,74 @@
+import chalk from 'chalk';
+import { Wallet, ethers } from 'ethers';
+import { appendFileSync } from 'fs';
+import moment from 'moment';
+import readlineSync from 'readline-sync';
+
+// Function to create a new Ethereum account
+function createAccountETH() {
+    const wallet = ethers.Wallet.createRandom();
+    const privateKey = wallet.privateKey;
+    const publicKey = wallet.publicKey;
+    const mnemonicKey = wallet.mnemonic.phrase;
+
+    return { privateKey, publicKey, mnemonicKey };
+}
+
+// Main function using async IIFE (Immediately Invoked Function Expression)
+(async () => {
+    try {
+        // Get the total number of wallets to create from user input
+        const totalWallet = readlineSync.question(
+            chalk.yellow('Input how much the wallet you want: ')
+        );
+
+        let count = 1;
+
+        // If the user entered a valid number greater than 1, set the count
+        if (totalWallet > 1) {
+            count = totalWallet;
+        }
+
+        // Create the specified number of wallets
+        while (count > 0) {
+            const createWalletResult = createAccountETH();
+            const theWallet = new Wallet(createWalletResult.privateKey);
+
+            if (theWallet) {
+                // Append wallet details to result.txt
+                appendFileSync(
+                    './address.txt',
+                    `${theWallet.address}\n`
+                );
+                appendFileSync(
+                    './privkey.txt', `${createWalletResult.privateKey}\n`
+                );
+                appendFileSync(
+                    './mnemonic.txt', `${createWalletResult.mnemonicKey}\n`
+                );
+                // Display success message with the wallet address and timestamp
+                console.log(
+                    chalk.green(
+                        `[${moment().format('HH:mm:ss')}] => ` + 'Wallet created...! Your address: ' + theWallet.address
+                    )
+                );
+            }
+
+            count--;
+        }
+
+        // Display final message after creating all wallets
+        // Change the timer (3000) if you think this is too long, in milliseconds.
+        setTimeout(() => {
+            console.log(
+                chalk.green(
+                    'All wallets have been created. Check your results in each file (address, mnemonic, and private key).'
+                )
+            );
+        }, 3000);
+        return;
+    } catch (error) {
+        // Display error message if an error occurs
+        console.log(chalk.red('Your program encountered an error! Message: ' + error));
+    }
+})();
